a61129ed0243467fb11eb7a7e631a0c595dea56c
BelyaevDm <dimabelyaev_2002@mail.ru>
2020-10-16 20:23:47 +0500

    Lab

--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -6,7 +6,35 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Напишите число, факториал которого хотите посчитать - ");
+            string aN = Console.ReadLine();
+            int aNumber = Convert.ToInt32(aN);
+            Console.Clear();
+            Console.WriteLine($"Для числа {aNumber}, факториалом является:\n");
+            int k = 1;
+            int k1 = -1;
+            int aFactorial = 1;
+            if (aNumber > 0)
+            {
+                for (int i = 0; k <= aNumber; k++)
+                {
+                    aFactorial = aFactorial * k;
+                    Console.Write($"| {aFactorial} ");
+                }
+            }
+            else if (aNumber == 0)
+            {
+                Console.Write($"| 0 ");
+            }
+            else
+            {
+                for (int i = 0; k1 >= aNumber; k1--)
+                {
+                    aFactorial = aFactorial * k1;
+                    Console.Write($"| {aFactorial} ");
+                }
+            }
+            Console.Write("|");
         }
     }
 }
